512a4d0840848950ed70248a5304aae67e4bd1a6
Jacques Nel <jmnel92@gmail.com>
2019-10-23 14:02:56 -0400

    Changed help browser instance from global to class variable. Made documentation path loading platform agnostic.

--- src/help_browser.py
+++ src/help_browser.py
@@ -3,11 +3,13 @@ from PySide2.QtWidgets import (QWidget, QPushButton, QTextBrowser, QHBoxLayout, 
         QDialog)
 from PySide2.QtCore import Qt, Slot
 
-browser = None  # Global reference to help browser instance
-
 class HelpBrowser(QWidget):
 
-    def __init__(self, path, page, parent=None):
+    instance = None         # Class variable instance of help browser
+    documentation_path = '' # Documentation path
+
+    def __init__(self, parent=None):
+#    def __init__(self, path, page, parent=None):
         # We don't pass parent to superclass, because we don't want help browser to be a child of
         # main window. We handle closing help browser when main window closes manually.
         super(HelpBrowser,self).__init__()
@@ -30,7 +32,6 @@ class HelpBrowser(QWidget):
         self.button_layout.addStretch()
         self.button_layout.addWidget(self.close_button)
 
-
         # Create basic layout containing QTextBrowser.
         self.text_browser = QTextBrowser()
         self.main_layout = QVBoxLayout()
@@ -43,38 +44,35 @@ class HelpBrowser(QWidget):
         self.back_button.clicked.connect(self.text_browser.backward)
         self.close_button.clicked.connect(self.close)
 
-        # Calls static function to clear global help browser instance reference.
+        # Calls static function to clear help browser instance reference.
         self.destroyed.connect(HelpBrowser.on_close)
-
-        # Get path of page and open it.
-        file_path_abs = path + '/' + page
-        self.text_browser.setSearchPaths(path)  # Not sure if this is needed
-        self.text_browser.setSource(file_path_abs)
 
         # Close help browser on parent is_closing signal.
         parent.is_closing.connect(self.close)
 
-    # Unsets global help browser instance reference. This gets called when help browser is
-    # destroyed.
+    # Navigates to page in documentation path.
+    def goto_page(self, page):
+        page_file_path = os.path.join(HelpBrowser.documentation_path, page)
+        self.text_browser.setSource(page_file_path)
+
+    # Sets documenation path.
+    @staticmethod
+    def set_documentation_path(path):
+        HelpBrowser.documentation_path = path
+
+    # Unsets help browser instance reference. This gets called when help browser is destroyed.
     @staticmethod
     def on_close():
-        global browser
-        browser = None
+        if HelpBrowser.instance != None:
+            HelpBrowser.instance = None
 
-    # Creates help browser window as global object.
+    # Creates and shows help browser window, stores instance in class variable, and navigates to
+    # page in documentation path.
     @staticmethod
-    def show_page( page, parent=None ):
-        # Get absolute path to 'docs' subdirectory.
-        path = os.path.abspath('docs/')
-        global browser
-        # Check that help browser doesn't already exist.
-        if browser == None:
-            # Create help browser and show it.
-            browser = HelpBrowser(path, page, parent)
-            browser.resize(500, 400)
-            browser.show()
-        else:
-            # Browser already open, so just change page.
-            file_path_abs = path + '/' + page
-            browser.text_browser.setSearchPaths(path)
-            browser.text_browser.setSource(file_path_abs)
+    def show_page(page, parent=None):
+        if HelpBrowser.instance == None:
+            HelpBrowser.instance = HelpBrowser(parent)
+            HelpBrowser.instance.resize(500,400)
+
+        HelpBrowser.instance.show()
+        HelpBrowser.instance.goto_page(page)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import sys
+import os
 from PySide2.QtWidgets import QApplication
 from main_window import MainWindow
+from help_browser import HelpBrowser
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     root_window = MainWindow()
     root_window.show()
+
+    # Get the directory of main.py and set documentation directory for help browser.
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    documentation_subdirectory = 'docs'
+    HelpBrowser.set_documentation_path(os.path.join( script_path, documentation_subdirectory))
+
+    # Launch application.
     sys.exit(app.exec_())
